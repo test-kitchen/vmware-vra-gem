463fc805e8d4ec5969e67fdc8ba85e05a76a4090
Oleg Shvartsman <oshvarts@ford.com>
2022-05-23 13:37:50 -0400

    Update catalog.rb

--- lib/vra/catalog.rb
+++ lib/vra/catalog.rb
@@ -15,7 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 module Vra
   class Catalog
@@ -52,7 +51,7 @@ module Vra
     def fetch_catalog_items(catalog_name)
       fetch_resources(
         Vra::CatalogItem,
-        "/catalog/api/admin/items",
+        "/catalog/api/items",
         "search=#{catalog_name}"
       )
     end
